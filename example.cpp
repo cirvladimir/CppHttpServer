bdb185a62a5c2b1cd10a8756d9bb97bf6ae8461f
Vladimir Korukov <v_korukov@hotmail.com>
2014-11-27 16:40:27 -0800

    Added post and get to server, added example

---
+++ example.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include "server.h"
+
+using namespace std;
+
+class MyHandler : public HTTPRequestHandler
+{
+public:
+    HTTPResponse handleRequest(HTTPRequest)
+    {
+	return HTTPResponse();
+    }
+};
+
+int main()
+{
+    
+}
--- server.h
+++ server.h
@@ -10,72 +10,142 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <string>
 
-class HTTPRequest {
+
+#define HTTP_SERVER_BUFFER_SIZE 1024
+
+class HTTPRequest 
+{
 public:
-    string Path;
-    string Method;
-    string Body;
+    std::string path;
+    std::string method;
+    std::string body;
 };
 
-class HTTPResponse {
+class HTTPResponse 
+{
 public:
-    int Code;
-    
+    int code;
+    std::string body;
+    std::string contentType;
 };
 
-class HTTPRequestHandler {
+class HTTPRequestHandler 
+{
 public:
-    virtual HTTPResponse HandleRequest(HTTPRequest request);
+    HTTPResponse handleRequest(HTTPRequest request) {}
 };
 
-class HTTPServer {
+class HTTPServer 
+{
 private:
     void error(const char *msg)
     {
 	perror(msg);
 	exit(1);
     }
+    HTTPRequestHandler handler;
+    int portno;
+    bool started;
+    int sockfd;
 public:
-    HTTPServer(
+    HTTPServer(HTTPRequestHandler hndl, int port) 
+    {
+	handler = hndl;
+	portno = port;
+	started = false;
+    }
+    void start()
+    {
+	started = true;
+	int newsockfd;
+	socklen_t clilen;
+	char buffer[HTTP_SERVER_BUFFER_SIZE];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) 
+	    error("ERROR opening socket");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+		sizeof(serv_addr)) < 0) 
+	    error("ERROR on binding");
+	listen(sockfd,5);
+	clilen = sizeof(cli_addr);
+	while (started)
+	{
+	    newsockfd = accept(sockfd, 
+			(struct sockaddr *) &cli_addr, 
+			&clilen);
+	    if (newsockfd < 0) 
+		error("ERROR on accept");
+	    bzero(buffer,HTTP_SERVER_BUFFER_SIZE);
+	    n = read(newsockfd,buffer,HTTP_SERVER_BUFFER_SIZE - 1);
+	    if (n < 0) error("ERROR reading from socket");
+	    HTTPRequest request;
+	    request.method = "";
+	    int ind = 0;
+	    while (buffer[ind] != ' ')
+		request.method += buffer[ind++];
+	    ind++;
+	    request.path = "";
+	    while (buffer[ind] != ' ')
+	    {
+		if (buffer[ind] == '%')
+		{
+		    char* endPtr = buffer + ind + 3;
+		    request.path += (char)strtol(buffer + ind + 1, 
+						 &endPtr, 16);
+		    ind += 3;
+		}
+		else
+		{
+		    request.path += buffer[ind++];
+		}
+	    }
+	    if (request.method == "POST")
+	    {
+		bool foundStart = false;
+		int numSlNs = 0;
+		while (!foundStart)
+		{
+		    if (buffer[ind] == '\n') {
+			numSlNs++;
+			if (numSlNs >= 2)
+			    foundStart = true;
+		    } else if ((buffer[ind] != '\r') && (buffer[ind] != ' '))
+			numSlNs = 0;
+		    ind++;
+		}
+		request.body = "";
+		for (; ind < n; ind++) {
+		    request.body += buffer[ind];
+		}
+		while (n == HTTP_SERVER_BUFFER_SIZE)
+		{
+		    ind = 0;
+		    n = read(newsockfd,buffer,HTTP_SERVER_BUFFER_SIZE - 1);
+		    for (; ind < n; ind++) {
+			request.body += buffer[ind];
+		    }
+		}
+	    }
+	    HTTPResponse response = handler.handleRequest(request);
+	    std::stringstream respStr;
+	    respStr << "HTTP/1.1 " << response.code << " OK\n" <<
+		"Content-Type: " << response.contentType << "; charset=utf-8\n" << 
+		"Content-Length: " << response.body.size() << "\n\n" << 
+		response.body;
+	    n = write(newsockfd, respStr.str().c_str(), respStr.gcount());
+	    //printf("Here is the message: %s\n",buffer);
+	    //n = write(newsockfd,"<html><body><form method='post'><button>go</button></form></body></html>                                                ",75);
+	    if (n < 0) error("ERROR writing to socket");
+	    close(newsockfd);
+	}
+	close(sockfd);
+    }
 };
-
-int main(int argc, char *argv[])
-{
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
-}
